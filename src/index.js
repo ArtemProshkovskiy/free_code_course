92664d250cea162c0f9fef6568a1c634578ec37e
artem <hajnkof@gmail.com>
2024-02-15 12:16:16 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Calculator from "./component/component";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Calculator></Calculator>);
